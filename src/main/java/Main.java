118895e19f5d6e69b7a48ddd80973396741cf6a0
dulleydoo <107411441+dulleydoo@users.noreply.github.com>
2022-07-02 12:19:09 -0400

    Completed Assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+      Main game = new Main();
+      game.sum();
     }
+    public int input(){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int number = inp.nextInt();
+        return number;
+    }
+    public void sum(){
+        int max = input();
+        int overall = 0;
+        for (int start = 0; start <= max; start++) {
+            overall += start;
+        }
+        System.out.println(overall);
+    }
+
 }
